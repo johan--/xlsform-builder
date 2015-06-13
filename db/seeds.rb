8c669e4a4412faffdd6ff6b8b074479e96291eab
Matthew White <matthew-white@users.noreply.github.com>
2015-06-13 19:11:06 -0400

    Seed the database with forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Form.create([
+  { form_title: 'My first form' },
+  { form_title: 'Form #2' }
+])
